6307738eccf59c3512d12c249a6853adf98c62f8
furkannkaymaz <furkannkaymaz@gmail.com>
2021-03-30 13:57:04 +0300

    edited

--- app/src/main/java/com/example/haber24/ui/MainActivity.kt
+++ app/src/main/java/com/example/haber24/ui/MainActivity.kt
@@ -28,12 +28,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         live()
         getNews()
 
     }
-
     fun getNews(){
         ServiceManager().getNews(
             {
